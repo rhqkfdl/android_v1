13b5ba5060140c44482c74709dc71d4d09e6ef73
rhqkfdl <nsh1044.naver.com>
2022-06-25 20:18:24 +0900

    0624 수업

---
+++ app/src/main/java/com/example/firstmyapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.firstmyapp;
+
+import android.os.Bundle;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import com.example.firstmyapp.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linear_layout_04);
+    }
+}
